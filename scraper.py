01a51ea0309b1231df9915acd2a275896422d095
Leif Greiss <helter1234@gmail.com>
2019-01-14 12:15:59 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,18 +3,19 @@ import urlparse
 import lxml.html
 #scrape_tablefunction:getspassedanindividualpagetoscrape
 def scrape_table(root):
-rows = root.cssselect("table.data tr") # selects all <tr> blocks within <table class=\
+        rows = root.cssselect("table.data tr") # selects all <tr> blocks within <table class=\
 "data">
-for row in rows:
+        for row in rows:
         # Set up our data record - we'll need it later
-record = {}
-table_cells = row.cssselect("td") if table_cells:
-            record['Artist'] = table_cells[0].text
-            record['Album'] = table_cells[1].text
-            record['Released'] = table_cells[2].text
-            record['Sales m'] = table_cells[4].text
+                record = {}
+                table_cells = row.cssselect("td") 
+                if table_cells:
+                        record['Artist'] = table_cells[0].text
+                        record['Album'] = table_cells[1].text
+                        record['Released'] = table_cells[2].text
+                        record['Sales m'] = table_cells[4].text
             # Print out the data we've gathered
-            print record, '------------'
+                        print record, '------------'
             # Finally, save the record to the datastore - 'Artist' is our unique key scraperwiki.datastore.save(["Artist"], record)
             #scrape_and_look_for_next_linkfunction:callsthescrape_table 
             #function,thenhuntsfora'next'link:ifoneisfound,callsitselfagai
